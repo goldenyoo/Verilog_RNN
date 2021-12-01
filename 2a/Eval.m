b98b6804f27abd4555751e93ad62abdfcf2fc291
goldenyoo <goldenyoo@kaist.ac.kr>
2021-12-01 17:27:25 +0900

    No feauture extraction

---
+++ 2a/Eval.m
@@ -0,0 +1,91 @@
+% ----------------------------------------------------------------------- %
+%    File_name: Calib.m
+%    Programmer: Seungjae Yoo                             
+%                                           
+%    Last Modified: 2020_03_30                           
+%                                                            
+ % ----------------------------------------------------------------------- %
+clear all
+clc
+
+data_labels = ['1', '2', '3', '4', '5', '6', '7', '8', '9']; 
+
+c1 = 0;
+c2 = 0;
+
+for data_label = data_labels
+
+FILENAME = strcat('D:\바탕화면\BCIIV_2a_mat\A0',data_label,'E_mat');
+load(FILENAME);
+
+FILENAME1 = strcat('D:\바탕화면\BCIIV_2a_mat\true_labels\A0',data_label,'E.mat');
+load(FILENAME1);
+
+%% 
+
+Class_unknown = [];
+Class_1 = [];
+Class_2 = [];
+Class_3 = [];
+Class_4 = [];
+
+
+i=1;
+while i <=length(h.EVENT.TYP)
+    if h.EVENT.TYP(i)== 783
+        Class_unknown = [Class_unknown h.EVENT.POS(i)];
+        i = i + 1;
+    else
+        i = i + 1;
+    end    
+end
+
+for i = 1:length(Class_unknown)
+    if classlabel(i) == 1
+        Class_1 = [Class_1 Class_unknown(i)];
+    elseif classlabel(i) == 2
+        Class_2 = [Class_2 Class_unknown(i)];
+    elseif classlabel(i) == 3
+        Class_3 = [Class_3 Class_unknown(i)];
+    else
+        Class_4 = [Class_4 Class_unknown(i)];
+    end
+
+end
+
+%% 
+for i = 1:length(Class_1)
+   XTest{i+c1,1} = my_normalization(s(Class_1(i):Class_1(i)+313,1:22))';
+   YTest(i+c1,1) = 1;
+end
+
+for i = 1:length(Class_2)
+   XTest{i+length(Class_1)+c1,1} = my_normalization(s(Class_2(i):Class_2(i)+313,1:22))';
+   YTest(i+length(Class_1)+c1,1) = 2;
+end
+
+c1 = c1 + length(Class_1) + length(Class_2);
+end
+YTest = categorical(YTest);
+
+
+%% 
+
+FILENAME = 'D:\바탕화면\Verilog RNN\my_git_folder\2a\net.mat';
+load(FILENAME);
+
+
+
+YPred = classify(net,XTest, ...
+    'SequenceLength','longest');
+
+acc = sum(YPred == YTest)./numel(YTest);
+disp(sprintf('Score: %f  ',acc));
+    
+%% 
+function n_signal = my_normalization(s)
+    Mean = mean(s);
+    Std = std(s);
+    
+    n_signal = (s - Mean)./Std;
+end
---
+++ 2a/Training.m
@@ -0,0 +1,51 @@
+clc
+close all
+clear all
+
+FILENAME = 'D:\바탕화면\Verilog RNN\my_git_folder\2a\Calib_data_a.mat';
+load(FILENAME);
+
+%% Using X1, X2 (Left hand vs Right hand)
+tmp_X = cat(3,X1,X2);
+tmp_Y = [Y1;Y2];
+
+idx = randperm(size(tmp_X,3));
+train_it = 1;
+dev_it = 1;
+
+for k = 1: size(tmp_X,3)
+    if k < size(tmp_X,3)*0.9
+        XTrain{train_it,1} = tmp_X(:,:,idx(k))';
+        YTrain(train_it,1) = tmp_Y(idx(k));
+        train_it = train_it + 1;
+    else 
+        XValidation{dev_it,1} = tmp_X(:,:,idx(k))';
+        YValidation(dev_it,1) = tmp_Y(idx(k));
+        dev_it = dev_it + 1;
+ 
+    end
+end
+YTrain = categorical(YTrain);
+YValidation = categorical(YValidation);
+
+%% 
+layers = [
+    sequenceInputLayer(22,"Name","sequence")
+    lstmLayer(314,"Name","lstm","OutputMode","last")
+    fullyConnectedLayer(2,"Name","fc")
+    softmaxLayer("Name","softmax")
+    classificationLayer("Name","classoutput")];
+
+options = trainingOptions('adam', ...
+    'ExecutionEnvironment','auto', ...
+    'LearnRateSchedule','piecewise', ...
+    'MiniBatchSize',64, ...
+    'MaxEpochs',60, ...
+    'ValidationData',{XValidation,YValidation}, ...
+    'Shuffle','every-epoch', ...   % "once', 'never', 'every-epoch'
+    'Verbose',1, ...
+    'Plots','training-progress')
+
+net = trainNetwork(XTrain,YTrain,layers,options);
+%% 
+save("D:\바탕화면\Verilog RNN\my_git_folder\2a\net.mat",'net');
